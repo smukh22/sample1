7872b7ef942e9d97ea61e9deeae791fa55635ae6
mukhso <mukhso@nlccu047.mol-it.com>
2020-12-18 13:50:52 +0530

    hh

---
+++ SimpleJava1/src/sample1/demo2.java
@@ -0,0 +1,22 @@
+package sample1;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class demo2 {
+
+	@BeforeTest
+	public void assert2()
+	{
+		
+	}
+	
+	
+	@Test
+	public void assert1()
+	{
+		
+	}
+	
+	
+}
